960b5e20a5a5bfa4ee501dadd0b9e5b5ac7b8ef8
robertogriel <roberto@griel.com.br>
2022-04-19 18:16:00 -0300

    Moved old admin

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
 
 
 //Custom Imports
@@ -13,16 +15,18 @@ const Domains = lazy(()=>import('./Components/Domains'));
 const PortfolioPage = lazy(()=>import('./Components/Portfolio'));
 
 
-const Admin = lazy(()=>import('./Components/Admin'));
-const Customers = lazy(()=>import('./Components/Admin/Contents/customers'));
-const CustomerForm = lazy(()=>import('./Components/Admin/Contents/edit-customer'));
-const ServiceForm = lazy(()=>import('./Components/Admin/Contents/edit-service'));
-const Services = lazy(()=>import('./Components/Admin/Contents/services'));
-const Contracts = lazy(()=>import('./Components/Admin/Contents/contracts'));
-const ContractForm = lazy(()=>import('./Components/Admin/Contents/edit-contract'));
-const Bills = lazy(()=>import('./Components/Admin/Contents/bills'));
-const BillForm = lazy(()=>import('./Components/Admin/Contents/edit-bill'));
+const AdminLTE = lazy(()=>import('./Components/AdminLTE'));
+const CustomersLTE = lazy(()=>import('./Components/AdminLTE/Contents/customers'));
+const CustomerFormLTE = lazy(()=>import('./Components/AdminLTE/Contents/edit-customer'));
+const ServiceFormLTE = lazy(()=>import('./Components/AdminLTE/Contents/edit-service'));
+const ServicesLTE = lazy(()=>import('./Components/AdminLTE/Contents/services'));
+const ContractsLTE = lazy(()=>import('./Components/AdminLTE/Contents/contracts'));
+const ContractFormLTE = lazy(()=>import('./Components/AdminLTE/Contents/edit-contract'));
+const BillsLTE = lazy(()=>import('./Components/AdminLTE/Contents/bills'));
+const BillFormLTE = lazy(()=>import('./Components/AdminLTE/Contents/edit-bill'));
 // const Contact = lazy(()=>import('./Components/Contact'));
+
+const Admin = lazy(()=>import('./Components/Admin'));
 
 
 
@@ -31,6 +35,7 @@ function App() {
 	  
 	  <Router>
 		<Suspense fallback={<Loading/>}>
+		<Header />
 		<Routes>
 			
 			<Route path='*' element={<NotFound/>}></Route>
@@ -47,30 +52,37 @@ function App() {
 
 			<Route exact path='/load' element={<Loading/>} />
 
-			<Route exact path='/sistema' element={<Admin />} />
+			<Route exact path='/admin' element={<Admin/>} >
 
-			<Route exact path='/sistema/clientes' element={<Customers  title="Clientes" />} />
-			
-			<Route exact path='/sistema/novo-cliente' element={<CustomerForm act="new" title="Criar Cliente" />} />
-			
-			<Route exact path='/sistema/editar-cliente-:id' element={<CustomerForm act="edit" title="Editar Cliente" />} />
 
-			<Route exact path='/sistema/servicos' element={<Services  title="Serviços" />} />
+			</Route>
 
-			<Route exact path='/sistema/novo-servico' element={<ServiceForm act="new"  title="Novo Serviço" />} />
 
-			<Route exact path='/sistema/editar-servico-:id' element={<ServiceForm act="edit"  title="Editar Serviço" />} />
 
-			<Route exact path='/sistema/contratos' element={<Contracts  title="Contratos" />} />
+				<Route exact path='/sistema' element={<AdminLTE />} />
 
-			<Route exact path='/sistema/novo-contrato' element={<ContractForm act="new"  title="Novo Contrato" />} />
-			
-			<Route exact path='/sistema/faturas' element={<Bills  title="Faturas" />} />
-			
-			<Route exact path='/sistema/nova-fatura' element={<BillForm act="new" title="Nova Fatura" />} />
+				<Route exact path='/sistema/clientes' element={<CustomersLTE title="Clientes" />} />
+				
+				<Route exact path='/sistema/novo-cliente' element={<CustomerFormLTE act="new" title="Criar Cliente" />} />
+				
+				<Route exact path='/sistema/editar-cliente-:id' element={<CustomerFormLTE act="edit" title="Editar Cliente" />} />
+
+				<Route exact path='/sistema/servicos' element={<ServicesLTE  title="Serviços" />} />
+
+				<Route exact path='/sistema/novo-servico' element={<ServiceFormLTE act="new"  title="Novo Serviço" />} />
+
+				<Route exact path='/sistema/editar-servico-:id' element={<ServiceFormLTE act="edit"  title="Editar Serviço" />} />
+
+				<Route exact path='/sistema/contratos' element={<ContractsLTE  title="Contratos" />} />
+
+				<Route exact path='/sistema/novo-contrato' element={<ContractFormLTE act="new"  title="Novo Contrato" />} />
+				
+				<Route exact path='/sistema/faturas' element={<BillsLTE  title="Faturas" />} />
+				
+				<Route exact path='/sistema/nova-fatura' element={<BillFormLTE act="new" title="Nova Fatura" />} />
 
 		</Routes>
-		
+		<Footer />
 		</Suspense>
 	</Router>
   );
