763bd433732680b9a1971da31ef455bdcbabfa38
Roberto Griel Filho <roberto@griel.com.br>
2022-12-06 20:19:42 -0300

    Changed return

--- pages/api/menu.ts
+++ pages/api/menu.ts
@@ -5,9 +5,8 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const get = async () => {
     const { MenuList } = await connect();
 
-    res.status(200).json(await MenuList.find({}));
+    return res.status(200).json(await MenuList.find({}));
 
-    return res.send(MenuList);
   };
 
   const { method } = req;
