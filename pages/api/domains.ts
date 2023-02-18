e208420f7ae43f6d5e8744c581b6e8b0f23bf49b
Roberto Griel Filho <roberto@griel.com.br>
2023-02-18 18:07:43 -0300

    API to get the domain information

---
+++ pages/api/domains.ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+const hander = async (req: NextApiRequest, res: NextApiResponse) => {
+  await fetch(
+    `api_here?d=${req.query.q}`,
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+
+      if (!data.res) {
+        throw new Error(
+          "Não foi possível fazer a consulta de disponibilidade. Tente novamente mais tarde."
+        );
+      }
+
+      return res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(400).json({
+        message: error.message
+      });
+    });
+};
+
+export default hander;
