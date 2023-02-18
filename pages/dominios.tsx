9e073c33090eb1b09909e609be94c2671e28dbf0
Roberto Griel Filho <roberto@griel.com.br>
2023-02-18 16:53:13 -0300

    Static visual for domain check and result

--- pages/dominios.tsx
+++ pages/dominios.tsx
@@ -1,21 +1,70 @@
+import { useState } from "react";
 import { HtmlHead } from "../components/Html/Head";
-import Section from "../components/Html/Section";
+import Main from "../components/Html/Main";
+import styles from "../styles/Domains.module.scss";
+import {  MdCancel, MdCheckCircle, MdSearch } from "react-icons/md";
 
 const Dominios: React.FC = () => {
+  const [domainInput, setDomainInput] = useState<string>("");
+
+  const checkDomain = async () => {
+    if (!domainInput) return;
+
+    //const domain = 'teste12.com.br'
+    const response = await fetch(`api/domains?q=${domainInput}`);
+    //const data = await response.json();
+  };
+
   return (
-    <Section>
+    <Main>
       <HtmlHead
         title="<Domínios /> ● Griel Developer"
-        metaDescription="Cheque a disponibilidade de um domínio na Internet"
+        metaDescription="Consulte aqui a disponibilidade de um domínio."
       />
 
-        <header>
-            <h1>Consulte um domínio</h1>
-        </header>
-
-    </Section>
+      <header className={styles.domains}>
+        <h1>Domínios</h1>
+        <p>Consulte aqui a disponibilidade de um domínio.</p>
+      </header>
+      <section className={styles.domainForm}>
+        <div className={styles.inputArea}>
+          <input
+            className={styles.input}
+            type="text"
+            placeholder="Ex.: www.dominio.com.br"
+            name="domain"
+            defaultValue={domainInput}
+            onChange={(e) => setDomainInput(e.target.value)}
+            autoComplete="off"
+            autoCorrect="off"
+            autoCapitalize="off"
+            spellCheck="false"
+          />
+          <button className={styles.button} onClick={checkDomain}><MdSearch fontSize={25} /></button>
+        </div>
+      </section>
+      <section className={styles.domainResult}>
+        <div className={styles.result}>
+          <div className={styles.resultIcon}>
+            <MdCheckCircle fontSize={50} color="var(--success)"/>
+          </div>
+          <div className={styles.resultText}>
+            <h2>dominio.com.br</h2>
+            <p>Disponível</p>
+          </div>
+        </div>
+        <div className={styles.result}>
+          <div className={styles.resultIcon}>
+            <MdCancel fontSize={50} color="var(--fail)" />
+          </div>
+          <div className={styles.resultText}>
+            <h2>dominio.com.br</h2>
+            <p>Indisponível</p>
+          </div>
+        </div>
+      </section>
+    </Main>
   );
 };
-
 
 export default Dominios;
