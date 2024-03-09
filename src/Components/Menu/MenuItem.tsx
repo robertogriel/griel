c9b0f15ead7a2d55b0d4a03a1c8b877cf3f92872
Roberto Griel Filho <roberto@griel.com.br>
2024-03-09 20:52:03 -0300

    Adjusting menu

--- src/Components/Menu/MenuItem.tsx
+++ src/Components/Menu/MenuItem.tsx
@@ -1,12 +1,10 @@
-/* eslint-disable @next/next/no-img-element */
-import Link from 'next/link'
 import React from 'react'
 import { MenuTypes } from '../../types/menu/Menu'
 
 export const MenuItem = ({ href, icon, alt, strong, small }: MenuTypes) => {
   return (
     <li>
-      <Link href={href}>
+      <a href={href}>
         <a>
           <img src={`/images/svg/menu-${icon}.svg`} alt={alt} />
           <div className="wrap">
@@ -14,7 +12,7 @@ export const MenuItem = ({ href, icon, alt, strong, small }: MenuTypes) => {
             <small>{small}</small>
           </div>
         </a>
-      </Link>
+      </a>
     </li>
   )
 }
